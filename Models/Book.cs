cdc07e300fdb5d0a87d109be655b7e447997224e
udivak <udivak9@gmail.com>
2024-11-20 11:34:41 +0200

    added Book class
    created Book class in Models

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+namespace eLibrary.Models;
+
+public class Book
+{
+    private string title;
+    private string author;
+    private string isbnNumber;
+    private int year;
+
+    public Book(string title, string author, string ISBN, int year)
+    {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.year = year;
+    }
+    
+    public string Title { get => title; set => title = value; }
+    public string Author { get => author; set => author = value; }
+    public string ISBN { get => isbnNumber; set => isbnNumber = value; }
+    public int Year { get => year; set => year = value; }
+}
